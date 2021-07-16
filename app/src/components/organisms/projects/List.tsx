8c65471a75837f33ff02ecefdd713aeb1cf59659
rewdaraw <rewdaraw@gmail.com>
2021-07-16 15:20:01 +0900

    wip: create ProjectList component

---
+++ app/src/components/organisms/projects/List.tsx
@@ -0,0 +1,12 @@
+import { Box, Link, VStack } from '@chakra-ui/react'
+import React from 'react'
+
+export const ProjectList = () => {
+  return (
+    <Link href="/">
+      <VStack rounded="lg" p={4}>
+        
+      </VStack>
+    </Link>
+  );
+}
